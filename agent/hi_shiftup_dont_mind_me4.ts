bd342dafab8ddafeb1243c577ccaf2ba0bf49488
qmengz <554318064@qq.com>
2025-11-18 20:23:18 +0800

    新增功能：增强 HTTP 请求和响应处理，支持 protobuf
    - 新增对 protobufjs 的依赖，以改进数据处理。
    
    - 实现将 Il2Cpp 字节数组转换为 JS Uint8Array 的函数。
    
    - 增强 HTTP 请求和响应的日志记录，包括 URI 和解码后的 JSON 数据。
    
    - 更新 System.Net.Http.HttpClient 的方法钩子，以捕获和解码请求和响应数据。

--- agent/hi_shiftup_dont_mind_me4.ts
+++ agent/hi_shiftup_dont_mind_me4.ts
@@ -1,4 +1,5 @@
 import "frida-il2cpp-bridge";
+import * as NK from "./NikkeMapping.js";
 
 const logFile = new File("E:\\Documents\\nikke\\NikkeLog.txt", "a+");
 function logToFile(message: string): void {
@@ -19,84 +20,164 @@ function getTimestamp() {
     return `${yyyy}-${MM}-${dd} ${hh}:${mm}:${ss}`;
 }
 
+// 将托管 byte[] 转换为 JS Uint8Array
+function toUint8Array(il2cppByteArray: Il2Cpp.Array<number>) {
+
+    let length = il2cppByteArray.length;
+    let buffer = new Uint8Array(length);
+
+    for (let i = 0; i < length; i++) {
+        buffer[i] = il2cppByteArray.get(i);
+    }
+    return buffer;
+}
+
 const delayss = 50;
-logToFile(`等待Dll加载中,等待时间: ${delayss} 秒 ...`);
+logToFile(`DLL loading, waiting time: ${delayss} seconds ...`);
 setTimeout(() => {
 
     Il2Cpp.perform(() => {
-        logToFile(`进入 Il2Cpp.perform() `);
+        logToFile(`Enter Il2Cpp.perform() `);
 
         // 获取dll 
-        const NK_Runtime = Il2Cpp.domain.assembly("NK.Runtime").image;
+        // const NK_Runtime = Il2Cpp.domain.assembly("NK.Runtime").image;
         const Net_Http = Il2Cpp.domain.assembly("System.Net.Http").image;
 
 
         // 获取类 
-        const ResponseDecryptor = NK_Runtime.class("NK.Network.PacketEncryption.ResponseDecryptor");
+        // const ResponseDecryptor = NK_Runtime.class("NK.Network.PacketEncryption.ResponseDecryptor");
         const Net_HttpClient = Net_Http.class("System.Net.Http.HttpClient");
+        // const Net_HttpRequestMessage = Net_Http.class("System.Net.Http.HttpRequestMessage");
+
+        // 获取所有方法
+        // Net_HttpRequestMessage.methods.forEach(m => {
+        //     console.log(`[method] - [${m.name}] - [${m.isStatic}] - ${m.toString()}`);
+        // });
+
 
         // 获取方法
-        const Decrypt = ResponseDecryptor.method("Decrypt", 1);
-        logToFile(`[Method] - [${Decrypt.name}] - ${Decrypt.toString()}`);
+        // const Decrypt = ResponseDecryptor.method("Decrypt", 1);
+        // logToFile(`[Method] - [${Decrypt.name}] - ${Decrypt.toString()}`);
 
         // Hook
-        Decrypt.implementation = function (encrypted: any): Il2Cpp.Array<number> {
-            logToFile(`[Hook] - [${Decrypt.name}] called`);
-            logToFile(`[Hook] - [${Decrypt.name}] 参数类型: ${encrypted.class.name}`);
+        // Decrypt.implementation = function (encrypted: any): Il2Cpp.Array<number> {
+        //     logToFile(`[Hook] - [${Decrypt.name}] called`);
+        //     logToFile(`[Hook] - [${Decrypt.name}] Input data type: ${encrypted.class.name}`);
 
-            // 调用原始方法
-            const result = this.method<Il2Cpp.Array<number>>("Decrypt", 1).invoke(encrypted);
+        //     // 调用原始方法
+        //     const result = this.method<Il2Cpp.Array<number>>("Decrypt", 1).invoke(encrypted);
 
-            const byteArray = result;
-            const hex = Array.from(byteArray).map(b =>
-                ('0' + (b & 0xff).toString(16)).slice(-2)
-            ).join('');
+        //     const byteArray = result;
+        //     const hex = Array.from(byteArray).map(b =>
+        //         ('0' + (b & 0xff).toString(16)).slice(-2)
+        //     ).join('');
 
-            logToFile(`[Hook] - [${Decrypt.name}] 返回数据 (hex): ${hex}`);
+        //     logToFile(`[Hook] - [${Decrypt.name}] Return data hex: ${hex}`);
 
-            // 返回结果
-            return result;
-        }
+        //     // 返回结果
+        //     return result;
+        // }
 
         Net_HttpClient.methods.forEach(nhc => {
             // hook
             // SendAsync(System.Net.Http.HttpRequestMessage request, System.Threading.CancellationToken cancellationToken);
             if (nhc.name.endsWith("Async") && !nhc.isStatic) {
                 try {
+                    let uri = "";
                     Interceptor.attach(nhc.virtualAddress, {
                         onEnter(args) {
-                            logToFile(`[Hook] - [${nhc.name}] called}`);
+                            logToFile(`[Hook] - [${nhc.name}] - [VA: ${nhc.virtualAddress}] called `);
                             const params = nhc.parameters;
                             for (let index = 0; index < params.length; index++) {
                                 // 检查参数类型
                                 try {
-                                    const paramaddr = args[index];
-                                    const addrprt = paramaddr.toString();
-                                    let uri = "";
-                                    if ("0x0" !== addrprt &&
-                                        (params[index].name === "cancellationToken" || params[index].name === "completionOption")) {
-                                        const base = ptr(addrprt);
-                                        const uriPtr = base.add(0x30).readPointer();
-                                        if (!uriPtr.isNull()) {
-                                            const strPtr = uriPtr.add(0x10).readPointer(); // Uri.m_String
-                                            if (!strPtr.isNull()) {
-                                                uri = strPtr.add(0x14).readUtf16String() ?? "";
+                                    const paramaddr = args[index + 1]; // Exclude args[0] = this
+
+                                    if (!paramaddr.isNull() && params[index].name === "request") {
+                                        try {
+                                            const request = new Il2Cpp.Object(paramaddr); // HttpRequestMessage
+                                            const content = request.method("get_Content").invoke() as Il2Cpp.Object;
+                                            const requestUri = request.method("get_RequestUri").invoke() as Il2Cpp.Object;
+                                            uri = requestUri.toString();
+
+                                            logToFile(`[Hook] - [${nhc.name}] - [VA: ${nhc.virtualAddress}] url: ${uri}`);
+                                            // const requestHeaders = request.method("get_Headers").invoke() as Il2Cpp.Object;
+
+                                            // ReadAsByteArrayAsync() -> Task<byte[]>
+                                            const byteTask = content.method("ReadAsByteArrayAsync").invoke() as Il2Cpp.Object;
+
+                                            // Task<byte[]>.Result
+                                            let byteArrObj = byteTask.method("get_Result").invoke() as Il2Cpp.Array<number>;
+                                            // 转成 Uint8Array
+                                            let rawBytes = toUint8Array(byteArrObj);
+                                            try {
+                                                Object.keys(NK.requestMap).forEach((key) => {
+                                                    if (uri.endsWith(key)) {
+                                                        const decodedRes = NK.requestMap[key].decode(rawBytes);
+                                                        logToFile(`[Hook] - [${nhc.name}] - [VA: ${nhc.virtualAddress}] - [uri:${key}] Input data json: ${JSON.stringify(decodedRes)}`);
+                                                        // return decoded;
+                                                    }
+                                                });
+                                                // const decodedRes = Nikke.ResGetSimRoom.decode(rawBytes);
+                                            } catch (dre) {
+                                                logToFile(`[Hook] - [${nhc.name}] - [VA: ${nhc.virtualAddress}] Input data protobuf exception: ${dre}`);
                                             }
+
+                                        } catch (dec) {
+
                                         }
-                                        logToFile(`[Hook] - [${nhc.name}] url: ${uri}`);
                                     }
                                 } catch (pe) {
-                                    logToFile(`[Hook] - [${nhc.name}] 输入值处理异常: ${pe} `);
+                                    logToFile(`[Hook] - [${nhc.name}] - [VA: ${nhc.virtualAddress}] Input data processing exception: ${pe} `);
                                 }
                             }
                         },
                         onLeave(retval) {
-                            logToFile(`[Hook] - [${nhc.name}] returned`);
+                            try {
+                                const taskObj = new Il2Cpp.Object(retval); // Task<HttpResponseMessage>
+
+                                // 调用 Task().GetAwaiter 拿到 AwaiterTask
+                                let awaiter = taskObj.method("GetAwaiter").invoke() as Il2Cpp.Object;
+
+                                // 调用 GetAwaiter().GetResult() 拿到 HttpResponseMessage
+                                let response = awaiter.method("GetResult").invoke() as Il2Cpp.Object;
+
+                                let statusCode = response.method("get_StatusCode").invoke() as Il2Cpp.Object;
+                                logToFile(`[Hook] - [${nhc.name}] - [VA: ${nhc.virtualAddress}] StatusCode: ${statusCode.toString()}`);
+
+                                let content = response.method("get_Content").invoke() as Il2Cpp.Object;
+
+                                // ReadAsByteArrayAsync() -> Task<byte[]>
+                                let byteTask = content.method("ReadAsByteArrayAsync").invoke() as Il2Cpp.Object;
+
+                                // Task<byte[]>.Result
+                                let byteArrObj = byteTask.method("get_Result").invoke() as Il2Cpp.Array<number>;
+
+                                // 转成 Uint8Array
+                                let rawBytes = toUint8Array(byteArrObj);
+                                try {
+                                    Object.keys(NK.responseMap).forEach((key) => {
+                                        if (uri.endsWith(key)) {
+                                            const decodedRes = NK.responseMap[key].decode(rawBytes);
+                                            logToFile(`[Hook] - [${nhc.name}] - [VA: ${nhc.virtualAddress}] - [uri:${key}] Return data json: ${JSON.stringify(decodedRes)}`);
+                                            // return decoded;
+                                        }
+                                    });
+                                    // const decodedRes = Nikke.ResGetSimRoom.decode(rawBytes);
+                                } catch (dre) {
+                                    logToFile(`[Hook] - [${nhc.name}] - [VA: ${nhc.virtualAddress}] Return data protobuf exception: ${dre}`);
+                                }
+
+                                uri = "";
+                            } catch (re) {
+                                logToFile(`[Hook] - [${nhc.name}] - [VA: ${nhc.virtualAddress}] Return data processing exception: ${re} `);
+                            }
+                            logToFile(`[Hook] - [${nhc.name}] - [VA: ${nhc.virtualAddress}] returned`);
                             return retval;
                         }
                     });
                 } catch (error) {
-                    logToFile(`[Hook] - [${nhc.name}] error: ${error}`);
+                    logToFile(`[Hook] - [${nhc.name}] - [VA: ${nhc.virtualAddress}] exception: ${error}`);
                 }
             }
 
